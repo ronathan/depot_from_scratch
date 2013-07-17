6639af50d917957320aed97b957e025864f6d299
ronathan <ronaldvictorino@gmail.com>
2013-07-17 21:46:49 +0000

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DepotFromScratch::Application.config.secret_key_base = '5cce98b3243bde831a9bedfc54b151dc4a6cd28c281b836e90c35af6f8fc2a7de6cf9d40c1487df5fc2e7f440abbb74ebedd0b47cd82809818744b7967e3033c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DepotFromScratch::Application.config.session_store :cookie_store, key: '_depot_from_scratch_session'
