e110fbd5b441a41488a94e8fe945ef89de76d785
ronathan <ronaldvictorino@gmail.com>
2013-07-18 15:39:44 +0000

    Added cart functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DepotFromScratch::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
